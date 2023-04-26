c60a44efb3ed7f9337e7667977849abd80c4e337
yavuzcobn <yvz.2012@gmail.com>
2023-04-26 22:00:14 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,5 +2,7 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir");
         //version1
+        System.out.println("Java is easy");
+        //version2
     }
 }
